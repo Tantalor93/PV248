1ef1eea55cbc35859f0e28dd8af45653c1098531
Ondřej Benkovský <ondrej.benkovsky@gooddata.com>
2017-10-15 14:54:16 +0200

    fill score_author

--- scorelib-import.py
+++ scorelib-import.py
@@ -25,7 +25,6 @@ class Person( DBItem ):
     def __init__( self, conn, string ):
         super().__init__( conn )
         self.name = re.sub( '\([0-9+-]+\)', '', string)
-        # TODO: years born/died
         rx = re.compile(r".*\((....)-(....)\)")
         rx2 = re.compile(r".*\((....)--(....)\)")
         m = rx.match(string)
@@ -49,16 +48,65 @@ class Person( DBItem ):
     def do_store( self ):
         self.cursor.execute( "insert into person (name, born, died) values (?,?,?)", (self.name, self.born, self.died) )
 
+
+class Score(DBItem):
+
+    def __init__(self, conn, genre, key, incipit, year):
+        super().__init__(conn)
+
+        self.genre = genre
+        self.key = key
+        self.incipit = incipit
+        self.year = year
+
+    def fetch_id(self):
+            self.cursor.execute( "select id from score where genre = ? AND key = ? AND incipit = ? AND year = ?", (self.genre, self.key, self.incipit, self.year) )
+            self.id = self.cursor.fetchone()
+
+    def do_store( self ):
+        self.cursor.execute( "insert into score (genre, key, incipit, year) values (?,?,?,?)", (self.genre, self.key, self.incipit, self.year) )
+
+class ScoreAuthor(DBItem):
+    def __init__(self, conn, score, composer):
+        super().__init__(conn)
+        self.score = score.id[0]
+        self.composer = composer.id[0]
+
+    def fetch_id(self):
+        self.cursor.execute( "select id from score_author where score = ? AND composer = ?", (self.score, self.composer) )
+        self.id = self.cursor.fetchone()
+
+    def do_store( self ):
+        self.cursor.execute( "insert into score_author (score, composer) values (?,?)", (self.score, self.composer) )
+
 conn = sqlite3.connect('scorelib.dat')
 rx = re.compile(r"(.*): (.*)")
-for line in open('scorelib.txt', 'r', encoding='utf-8'):
-    m = rx.match(line)
-    if m is None: continue
-    k = m.group(1)
-    v = m.group(2)
-    if k == 'Composer':
-        for c in v.split(';'):
-            p = Person(conn, c.strip())
-            p.store()
+
+fileAsString = open('scorelib.txt', 'r', encoding='utf-8').read()
+scores = fileAsString.split('\n\n')
+
+for scoreString in scores:
+    for line in scoreString.split('\n'):
+        match = rx.match(line)
+        if match is None: continue
+        k = match.group(1)
+        v = match.group(2)
+        if k == 'Genre':
+            genre = v
+        if k == 'Key':
+            key = v
+        if k == 'Incipit':
+            incipit = v
+        if k == 'Composition Year':
+            year = v
+        if k == 'Composer':
+            for c in v.split(';'):
+                person = Person(conn, c.strip())
+                person.store()
+
+    score = Score(conn, genre, key, incipit, year)
+    score.store()
+    scoreAuthor = ScoreAuthor(conn, score, person)
+    scoreAuthor.store()
 
 conn.commit()
