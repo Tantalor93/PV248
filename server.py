c7dde06cd85b0790510934b83f67c24cfb195c35
Ondřej Benkovský <ondrej.benkovsky@gooddata.com>
2017-11-02 18:33:23 +0100

    exercise 5 server

---
+++ server.py
@@ -0,0 +1,18 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type','text/html')
+        self.end_headers()
+        self.wfile.write(bytes("<html><body><h1>you asked for " + self.path + "</h1></body></html>", encoding="utf-8"))
+
+
+def run(server_class=HTTPServer, handler_class=Handler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+run()
