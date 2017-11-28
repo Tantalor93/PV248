8b4833eaa32abd04db7f4f99518e5d60e9958a22
Ondřej Benkovský <ondrej.benkovsky@gooddata.com>
2017-11-28 14:25:31 +0100

    more work

--- exercise6.py
+++ exercise6.py
@@ -1,9 +1,22 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 import sqlite3
+import json
 
 
 class Handler(BaseHTTPRequestHandler):
+
+    def do_search(self, composer):
+        conn = sqlite3.connect('scorelib.dat')
+        cursor = conn.cursor()
+        result = cursor.execute("SELECT person.name, score.name FROM person JOIN score_author on person.id = score_author.composer JOIN score on score_author.score = score.id WHERE person.name LIKE ? ", ("%" + composer + "%",)).fetchall()
+
+        dict = {}
+        for i in result:
+            if i[0] not in dict:
+                dict[i[0]] = list()
+            dict[i[0]].append(i[1])
+        return dict;
 
     def do_GET(self):
         parsed = urlparse(self.path)
@@ -11,6 +24,12 @@ class Handler(BaseHTTPRequestHandler):
             qs = parse_qs(parsed.query)
             searchTerm = qs['q'][0]
             format = qs['f'][0]
+            result = self.do_search(searchTerm)
+            if format == 'json':
+                jsonResult = json.dumps(result)
+                self.send_response(200)
+                self.wfile.write(bytes(jsonResult, encoding="utf-8"))
+
 
 
 def run(server_class=HTTPServer, handler_class=Handler, port=8000):
---
+++ exercise8.py
@@ -0,0 +1,2 @@
+from pygame import display, draw, time, event
+screen = display.set_mode( [800, 600] )
